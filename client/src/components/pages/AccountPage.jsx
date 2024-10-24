5356ac472ad47c709fcbe9b2ee65047e28bbbab5
Elizaveta_Vadimovna <Lizichk@mail.ru>
2024-10-24 16:56:51 +0300

    шаблон аккаунта

--- client/src/components/pages/AccountPage.jsx
+++ client/src/components/pages/AccountPage.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 export default function AccountPage() {
   return (
-    <div>AccountPage</div>
-  )
+    <div>
+      <h1>Личный кабинет пользователя {user}</h1>
+      <div className="cardOfUser">
+        <div className="userName">userName</div>
+        <Button size="sm" className="userNameButton">
+          userName
+        </Button>
+        <div className="userEmail">userEmail</div>
+        <div className="userHashPass">userHashPass</div>
+        <Button size="sm" className="userHashPassButton">
+          userHashPass
+        </Button>
+        <div className="userProgress">userProgress</div>
+        <div className="userGroups">userGroups</div>
+        <div className="userIsAdmin">userIsAdmin</div>
+      </div>
+    </div>
+  );
 }
